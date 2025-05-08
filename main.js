d2926a09f1ea7dffc7f46b699cf4b49fa9883cff
jenwit-dev <johnny.js19@gmail.com>
2025-05-08 10:27:30 +0700

    learn: call stack and setTimeout

--- main.js
+++ main.js
@@ -13,3 +13,34 @@ function c() {
 }
 
 c();
+
+function a() {
+  console.log("a");
+}
+
+function b() {
+  setTimeout(() => {
+    for (let i = 0; i < 10000000; i++) {}
+    console.log("b");
+  }, 0);
+}
+
+function c() {
+  console.log("c");
+}
+
+a();
+b();
+c();
+
+// setTimeout(() => {
+//   console.log("execute 3");
+// }, 2000);
+// console.log("before");
+// setTimeout(() => {
+//   console.log("execute 1");
+// }, 0);
+// console.log("after");
+// setTimeout(() => {
+//   console.log("execute 2");
+// }, 1000);
