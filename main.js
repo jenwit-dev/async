bc66ecc17d34daeefb10805356d47ee26c3d0e38
jenwit-dev <johnny.js19@gmail.com>
2025-05-10 16:42:00 +0700

    finish:async/await

--- main.js
+++ main.js
@@ -91,39 +91,39 @@ const { readFile } = require("fs");
 
 // console.log(parsedData1.firstName + ":" + parsedData2.name);
 
-function a() {
-  console.log("a");
-}
+// function a() {
+//   console.log("a");
+// }
 
-function b(age) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      for (let i = 0; i < 10_000_000; i++) {}
-      console.log("b");
-      if (age >= 18) {
-        resolve("success");
-      } else {
-        reject("less than 18");
-      }
-    }, 0);
-  });
-  return promise;
-}
+// function b(age) {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       for (let i = 0; i < 10_000_000; i++) {}
+//       console.log("b");
+//       if (age >= 18) {
+//         resolve("success");
+//       } else {
+//         reject("less than 18");
+//       }
+//     }, 0);
+//   });
+//   return promise;
+// }
 
-function c() {
-  console.log("c");
-}
+// function c() {
+//   console.log("c");
+// }
 
-function d(input) {
-  console.log(input);
-}
+// function d(input) {
+//   console.log(input);
+// }
 
 // a();
 // b(20);
 // c();
 // d(b(20)); // Promise { <pending> }
 
-const p = b(20);
+// const p = b(20);
 // p.then((response) => {
 //   console.log(response);
 // });
@@ -132,12 +132,67 @@ const p = b(20);
 //   console.log(err);
 // });
 
-p.then((response) => {
-  console.log(response);
-})
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    console.log("finally");
+// p.then((response) => {
+//   console.log(response);
+// })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     console.log("finally");
+//   });
+function readFilePromise(file) {
+  const p = new Promise((resolve, reject) => {
+    readFile(file, "utf-8", (err, data) => {
+      if (data) {
+        return resolve(data);
+      } else {
+        return reject(err);
+      }
+    });
   });
+  return p;
+}
+
+// readFilePromise("user.json")
+//   .then((response) => {
+//     console.log(response);
+//     return 555;
+//   })
+//   .then((response555) => {
+//     console.log(response555);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const p1 = readFilePromise("user.json");
+// const p2 = readFilePromise("product.json");
+
+// const resultPromiseAll = Promise.all([p1, p2]);
+
+// resultPromiseAll
+//   .then((response) => {
+//     const [resUser, resProduct] = response;
+//     console.log(
+//       JSON.parse(resUser).firstName + ":" + JSON.parse(resProduct).name
+//     );
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function run() {}
+
+// const run = async function () {};
+
+const run = async () => {
+  try {
+    const p1 = await readFilePromise("user.jso");
+    const p2 = await readFilePromise("product.json");
+    console.log(JSON.parse(p1).firstName + ":" + JSON.parse(p2).name);
+  } catch (err) {
+    console.log(err);
+  }
+};
+run();
